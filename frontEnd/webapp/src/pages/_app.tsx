27feb6f925badfe22b43cd03210e24fcb98015b6
Manuel Dithurbide <mdithurbide@itba.edu.ar>
2023-04-01 01:37:09 -0300

    feat: landing page development

--- frontEnd/webapp/src/pages/_app.tsx
+++ frontEnd/webapp/src/pages/_app.tsx
@@ -1,6 +1,133 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import '../styles/globals.css'
+import {
+  createStyles,
+  Header,
+  Group,
+  Button,
+  Box,
+  Burger,
+  rem,
+} from '@mantine/core';
+import { MantineLogo } from '@mantine/ds';
+import { useDisclosure } from '@mantine/hooks';
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan('sm')]: {
+      height: rem(42),
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+    },
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    }),
+  },
+
+  subLink: {
+    width: '100%',
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+    borderRadius: theme.radius.md,
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    }),
+
+    '&:active': theme.activeStyles,
+  },
+
+  dropdownFooter: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    margin: `calc(${theme.spacing.md} * -1)`,
+    marginTop: theme.spacing.sm,
+    padding: `${theme.spacing.md} calc(${theme.spacing.md} * 2)`,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export function HeaderMegaMenu() {
+  const [drawerOpened, { toggle: toggleDrawer }] = useDisclosure(false);
+  const { classes } = useStyles();
+
+  return (
+    <Box pb={0}>
+      <Header height={60} px="md">
+        <Group position="apart" sx={{ height: '100%' }}>
+          <MantineLogo size={30} />
+
+          {/* <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
+            <a href="#" className={classes.link}>
+              Tag 1
+            </a>
+            <a href="#" className={classes.link}>
+              Tag 2
+            </a>
+            <a href="#" className={classes.link}>
+              Tag 3
+            </a>
+          </Group> */}
+
+          <Group className={classes.hiddenMobile}>
+            <Button>
+              Connect Wallet
+            </Button>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} className={classes.hiddenDesktop} />
+        </Group>
+      </Header>
+
+    </Box>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Trust Chain</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+    </Head>
+      <HeaderMegaMenu></HeaderMegaMenu>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light', 
+        }}
+      >
+      <Component {...pageProps} />
+      </MantineProvider>
+  </>
+  )
 }
