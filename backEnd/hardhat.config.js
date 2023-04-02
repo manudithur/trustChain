81fca38f9696a13d52e086352e8bbbd160b981cf
TobiasPerry <tperry@itba.edu.ar>
2023-04-02 00:26:57 -0300

    changes to deploy to polygon

--- backEnd/hardhat.config.js
+++ backEnd/hardhat.config.js
@@ -1,6 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+//antes la version e solidity era 0.8.9
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config();
+const { PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: "0.8.18",
-};
+  defaultNetwork: "PolygonMumbai",
+  solidity: '0.8.9',
+  networks: {
+    hardhat: {
+    },
+    PolygonMumbai : {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
--- backEnd/scripts/deployContract.js
+++ backEnd/scripts/deployContract.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat");
 
 
 async function deployContract() {
-    const ExampleNFT = await ethers.getContractFactory("AgreementContract") 
+    const ExampleNFT = await ethers.getContractFactory("Arbitrator") 
     const exampleNFT = await ExampleNFT.deploy()
     await exampleNFT.deployed()
     const txHash = exampleNFT.deployTransaction.hash
